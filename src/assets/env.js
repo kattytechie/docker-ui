af2b0736a4cf681d9b12a096320458923138a1c3
Koshlendra2017 <koshlendra@zibal.com>
2023-06-24 10:01:24 +0530

    Docker UI commit

---
+++ src/assets/env.js
@@ -0,0 +1,8 @@
+(function (window) {
+    window["env"] = window["env"] || {};
+
+    // Environment variables
+    window["env"]["production"] = false;
+    window["env"]["scrumAppUrl"] = 'http://localhost:8080';
+   
+})(this);
